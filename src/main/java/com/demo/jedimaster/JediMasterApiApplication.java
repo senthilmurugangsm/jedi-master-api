cec5f7244a4f52c65bde30ea61fd6a78199493a0
Senthil Murugan <senthilmurugangsm@gmail.com>
2023-05-04 02:20:40 +0800

    Project initial config

---
+++ src/main/java/com/demo/jedimaster/JediMasterApiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.jedimaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JediMasterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JediMasterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/jedimaster/JediMasterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.jedimaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JediMasterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
